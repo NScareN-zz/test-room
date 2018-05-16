bc7fd8bfabb884e764d40e94c74a375cf4eb2864
Sergei <37381458+NScareN@users.noreply.github.com>
2018-05-16 22:12:07 +0700

    Add files via upload
    Just a few updates in code and GitHub Extension for Visual Studio connection

--- TestBot/Модули/Hello.cs
+++ TestBot/Модули/Hello.cs
@@ -1,6 +1,7 @@
 ﻿using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,24 @@ namespace TestBot.Модули
         [Command("Виталя, привет")]
         public async Task HelloAsync()
         {
+            int a = 10;
             await ReplyAsync("Привет!");
+            await ReplyAsync($"{a}");
+            /*string path = @"D:\test.txt";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                File.Create(@"D:\test2.txt");
+                string path2 = @"D:\test2.txt";
+                FileInfo fileInf2 = new FileInfo(path);
+                if (fileInf2.Exists)
+                {
+                    string text = Console.ReadLine();
+                    File.WriteAllText();
+                }
+            }*/
+            string path = @"D:\test.txt"; //Get a path to the file
+            File.WriteAllText(path, "Test"); //Creates file, writes a text and closes the file. And it works!!!
         }
     }
 }
